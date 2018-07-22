64d6d89007e927bb68338b2476f788312545c98f
Ayhan Akilli <info@ayhan.biz>
2018-07-22 02:10:40 +0200

    Add some config

--- editor/index.js
+++ editor/index.js
@@ -2,7 +2,10 @@ import Editor from '../../editor/src/Editor.js';
 
 (function (document, Editor) {
     document.addEventListener('DOMContentLoaded', () => {
-        const editor = Editor.create(document.getElementById('editor'));
+        const editor = Editor.create(document.getElementById('editor'), {
+            mediabrowser: '/rte/browser',
+            path: '/editor/src/'
+        });
         const save = document.getElementById('save');
 
         save.addEventListener('click', (ev) => {
