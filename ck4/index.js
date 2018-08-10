ea0ba357e0d217c41afb03d0a50b37b21008bfe9
Ayhan Akilli <info@ayhan.biz>
2018-08-10 13:39:52 +0200

    Simplify setup

--- ck4/index.js
+++ ck4/index.js
@@ -2,16 +2,33 @@
 
 (function (document, CKEDITOR) {
     document.addEventListener('DOMContentLoaded', () => {
+        CKEDITOR.plugins.addExternal('detail', '../../../ckeditor4-detail/');
+        CKEDITOR.plugins.addExternal('media', '../../../ckeditor4-media/');
+        CKEDITOR.plugins.addExternal('mediabrowser', '../../../ckeditor4-mediabrowser/');
         const editor = CKEDITOR.replace(document.getElementById('rte'), {
             contentsCss: '',
             customConfig: '',
             disableNativeSpellChecker: true,
+            extraPlugins: 'detail,media,mediabrowser',
             fillEmptyBlocks: false,
             format_tags: 'p;h2;h3',
             height: '30rem',
             language: 'de',
             mediabrowserUrl: '../browser/',
             removeDialogTabs: 'link:advanced;link:target',
+            plugins:
+                'basicstyles,' +
+                'blockquote,' +
+                'divarea,' +
+                'enterkey,' +
+                'floatingspace,' +
+                'format,' +
+                'link,' +
+                'list,' +
+                'magicline,' +
+                'table,' +
+                'toolbar,' +
+                'undo',
             stylesSet: false,
             toolbar: [
                 {
